2c8eebc8b00bf7e641c5744b29dbc53cb37cd710
NimeshAlwis <nimeshalwis13@gmail.com>
2026-04-17 17:03:47 +0530

    create project structure

--- src/main/java/com/sliit/paf/repository/ResourceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sliit.paf.repository;
-
-import com.sliit.paf.model.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResourceRepository extends JpaRepository<Resource, Long> {
-}
