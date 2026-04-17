7372732bfe2c2ce5cd9fcd95b86cb2f985801109
NimeshAlwis <nimeshalwis13@gmail.com>
2026-04-17 13:19:10 +0530

    first commit

---
+++ src/main/java/com/we87/paf/PafApplication.java
@@ -0,0 +1,13 @@
+package com.we87.paf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/we87/paf/PafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.we87.paf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
